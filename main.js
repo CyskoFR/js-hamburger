2dccebf21cbbb7a7739393f13e631587a9af2026
CyskoFR <franc.sko91@gmail.com>
2022-04-29 16:03:04 +0200

    add id + fix function

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const hamburgerMenu = document.getElementsByClassName("hamburger-menu")
+const hamburgerMenu = document.getElementById("hamburger_menu")
 const openMenu = document.getElementById("open-menu");
 const closeMenu = document.getElementById("close-menu");
 
 openMenu.addEventListener("click", hamburgerOn);
 
+closeMenu.addEventListener("click", hamburgerOff);
+
 function hamburgerOn() {
     hamburgerMenu.style.display = "block";
+}
+
+function hamburgerOff() {
+    hamburgerMenu.style.display = "none";
 }
